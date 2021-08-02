1d359a20dc71b83cb0b6f181636c357b9ea1c337
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2021-08-02 19:22:31 +0300

    Login and registration added

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from flask import render_template
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+import routes
---
+++ routes.py
@@ -0,0 +1,36 @@
+from app import app
+from flask import render_template, request, redirect
+import users
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return None
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.register(username, password):
+            return redirect("/")
+        else:
+            return None
