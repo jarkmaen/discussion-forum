611f98e1a40ef47f24988ea7be76becc0bdc2c3e
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2021-08-03 13:26:38 +0300

    Requirements and schema added

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT
+);
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    topic_id INTEGER REFERENCES topics,
+    user_id INTEGER REFERENCES users,
+    title TEXT,
+    content TEXT
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    post_id INTEGER REFERENCES posts,
+    user_id INTEGER REFERENCES users,
+    content TEXT
+);
