90e4e4a123d8af0c1d4097e80265ece925d5ee76
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-09-22 23:40:22 +0300

    Switched db.py to local sqlite only

--- db.py
+++ db.py
@@ -1,9 +1,6 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
 
-uri = getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///local.db"
+
 db = SQLAlchemy(app)
