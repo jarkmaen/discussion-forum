2b217bfa15fff43225d2c7bc8916ed306fb85787
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-09-23 23:56:42 +0300

    Switched database from SQLite to PostgreSQL, updated schema and dependencies

--- db.py
+++ db.py
@@ -1,6 +1,39 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from sqlalchemy import inspect, text
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///local.db"
+uri = getenv("DATABASE_URI")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+# app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
+
+with app.app_context():
+    # Initialize tables if needed
+    inspector = inspect(db.engine)
+
+    if not inspector.has_table("users"):
+        with open("schema.sql", "r") as f:
+            schema_sql = f.read()
+
+        for sql in schema_sql.split(";"):
+            statement = sql.strip()
+
+            if statement:
+                db.session.execute(text(statement))
+
+    db.session.commit()
+
+    # Ensure admin user exists
+    sql = text("SELECT id FROM users WHERE username = :username")
+    admin = db.session.execute(sql, {"username": "admin"}).fetchone()
+
+    # Hash for password "admin". Generated with werkzeug.security.generate_password_hash
+    admin_password = "scrypt:32768:8:1$QoKhaEQ9g3YYP39X$220e703ee37255084fb1117c2b575792a78e203f99c2f4a086c49950c4f0ac8f43e0d9fc5e17740c40061e4ed78f488949cb7e2b4a9bce88f4a8d20f482f0b26"
+
+    if not admin:
+        sql = text("INSERT INTO users (username, password, is_admin) VALUES (:username, :password, TRUE)")
+        db.session.execute(sql, {"username": "admin", "password": admin_password})
+        db.session.commit()
--- schema.sql
+++ schema.sql
@@ -1,35 +1,35 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE topics (
     id SERIAL PRIMARY KEY,
-    name TEXT,
+    title VARCHAR(255) NOT NULL,
     private BOOLEAN DEFAULT FALSE,
     visible BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    topic_id INTEGER REFERENCES topics,
-    user_id INTEGER REFERENCES users,
-    title TEXT,
-    content TEXT,
+    topic_id INTEGER NOT NULL REFERENCES topics(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
     visible BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
-    post_id INTEGER REFERENCES posts,
-    user_id INTEGER REFERENCES users,
-    content TEXT
+    post_id INTEGER NOT NULL REFERENCES posts(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    content TEXT NOT NULL
 );
 
 CREATE TABLE private_topics (
     id SERIAL PRIMARY KEY,
-    topic_id INTEGER REFERENCES topics,
-    user_id INTEGER REFERENCES users
+    topic_id INTEGER NOT NULL REFERENCES topics(id),
+    user_id INTEGER NOT NULL REFERENCES users(id)
 );
